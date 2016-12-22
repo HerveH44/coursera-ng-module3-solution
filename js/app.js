f386af505a855861caf567eb5ef2b12e5f71602c
HerveH44 <herve.huneau@gmail.com>
2016-12-22 10:02:27 +0100

    delete logs

--- js/app.js
+++ js/app.js
@@ -1,87 +1,88 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular.module('NarrowItDownApp', [])
-  .controller('NarrowItDownController', NarrowItDownController)
-  .service('MenuSearchService', MenuSearchService)
-  .directive('foundItems', foundItemsDirective)
-  .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', foundItemsDirective)
+        .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
 
-  function foundItemsDirective() {
-    var ddo = {
-      restrict: 'E',
-      templateUrl: 'directive/foundItems.html',
-      scope: {
-        foundItems: '<',
-        onRemove: '&',
-        noItemFound: '<'
-      },
-      controller: foundItemsDirectiveController,
-      controllerAs: 'list',
-      bindToController: true
-    };
+    function foundItemsDirective() {
+        var ddo = {
+            restrict: 'E',
+            templateUrl: 'directive/foundItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&',
+                noItemFound: '<'
+            },
+            controller: foundItemsDirectiveController,
+            controllerAs: 'list',
+            bindToController: true
+        };
 
-    return ddo;
-  }
-
-  function foundItemsDirectiveController() {
-    var list = this;
-
-    list.buttonTitle = "Don't want this one!";
-    list.noResultTitle = "Nothing found";
-  }
-
-
-  NarrowItDownController.$inject = ['MenuSearchService'];
-  function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
-
-    ctrl.title = "Narrow Down Your Chinese Menu Choice";
-    ctrl.buttonTitle = "Narrow It Down For Me!";
-
-    ctrl.searchTerm = "";
-    ctrl.found = [];
-    ctrl.noItemFound = false;
-
-    ctrl.getMatchedMenuItems = function () {
-      MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
-      .then(function(response){
-        ctrl.found = response;
-        ctrl.noItemFound = ctrl.found.length == 0;
-        console.log("ctrl.noItemFound:", ctrl.noItemFound);
-      });
+        return ddo;
     }
 
-    ctrl.onRemove = function (index) {
-      ctrl.found.splice(index, 1);
+    function foundItemsDirectiveController() {
+        var list = this;
+
+        list.buttonTitle = "Don't want this one!";
+        list.noResultTitle = "Nothing found";
     }
-  }
 
-  MenuSearchService.inject = ['$http', 'ApiBasePath'];
-  function MenuSearchService($http, ApiBasePath) {
-    var service = this;
 
-    service.getMatchedMenuItems = function(searchTerm) {
-      return service.getMenuItems()
-      .then(function (result) {
-          service.menuItems = result.data.menu_items;
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var ctrl = this;
 
-          function filterMenu(el) {
-              return searchTerm && el.description.indexOf(searchTerm) !== -1;
-          }
+        ctrl.title = "Narrow Down Your Chinese Menu Choice";
+        ctrl.buttonTitle = "Narrow It Down For Me!";
 
-          var foundItems = service.menuItems.filter(filterMenu);
+        ctrl.searchTerm = "";
+        ctrl.found = [];
+        ctrl.noItemFound = false;
 
-          // return processed items
-          return foundItems;
-      });
-    };
+        ctrl.getMatchedMenuItems = function() {
+            MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
+                .then(function(response) {
+                    ctrl.found = response;
+                    ctrl.noItemFound = ctrl.found.length == 0;
+                });
+        }
 
-    service.getMenuItems = function() {
-      return service.result = service.result || $http({
-        method: "GET",
-        url: (ApiBasePath + "/menu_items.json"),
-      });
+        ctrl.onRemove = function(index) {
+            ctrl.found.splice(index, 1);
+        }
     }
-  }
+
+    MenuSearchService.inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return service.getMenuItems()
+                .then(function(result) {
+                    service.menuItems = result.data.menu_items;
+
+                    function filterMenu(el) {
+                        if(!searchTerm)
+                          return false;
+                        return el.description.indexOf(searchTerm) !== -1;
+                    }
+
+                    var foundItems = service.menuItems.filter(filterMenu);
+
+                    // return processed items
+                    return foundItems;
+                });
+        };
+
+        service.getMenuItems = function() {
+            return service.result = service.result || $http({
+                method: "GET",
+                url: (ApiBasePath + "/menu_items.json"),
+            });
+        }
+    }
 })();
