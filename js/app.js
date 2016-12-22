ddcb8a69560d953575893338ec791ea528604a3f
HerveH44 <herve.huneau@gmail.com>
2016-12-22 12:33:58 +0100

    change into table,add sorters

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
+/**
+ * Created by hhuneau on 21/12/2016.
+ * Reformat thanks to dbourgon
+ */
+
+// IIFE
 (function() {
     'use strict';
 
+    // Definitions
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
         .directive('foundItems', foundItemsDirective)
         .constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
 
+    ///////////////
+    // DIRECTIVE:  FoundItemsDirective
+    //////////////
     function foundItemsDirective() {
         var ddo = {
             restrict: 'E',
@@ -14,7 +24,9 @@
             scope: {
                 foundItems: '<',
                 onRemove: '&',
-                noItemFound: '<'
+                noItemFound: '<',
+                searching: '<isSearching',
+                finished: '<isFinished'
             },
             controller: foundItemsDirectiveController,
             controllerAs: 'list',
@@ -24,39 +36,87 @@
         return ddo;
     }
 
+    ///////////////
+    // DIRECTIVE CTRL:  FoundItemsDirective
+    //////////////
     function foundItemsDirectiveController() {
         var list = this;
 
         list.buttonTitle = "Don't want this one!";
         list.noResultTitle = "Nothing found";
+
+        list.sortOrder = 'id';
+        list.sortReverse = false;
+
+        list.headers = [{
+            lib: "Id.",
+            id: "id"
+        },{
+            lib: "Short name",
+            id: "short_name"
+        },{
+            lib: "Name",
+            id: "name"
+        },{
+            lib: "Description",
+            id: "description"
+        },{
+            lib: "Price small",
+            id: "price_small"
+        },{
+            lib: "Price large",
+            id: "price_large"
+        }];
     }
 
-
+    ///////////////
+    // CONTROLLER:     NarrowItDownController
+    //////////////
     NarrowItDownController.$inject = ['MenuSearchService'];
+
     function NarrowItDownController(MenuSearchService) {
-        var ctrl = this;
+        var main = this;
 
-        ctrl.title = "Narrow Down Your Chinese Menu Choice";
-        ctrl.buttonTitle = "Narrow It Down For Me!";
+        main.title = "Narrow Down Your Chinese Menu Choice";
+        main.buttonTitle = "Narrow It Down For Me!";
 
-        ctrl.searchTerm = "";
-        ctrl.found = [];
-        ctrl.noItemFound = false;
+        main.searchTerm = "";
+        main.found = [];
+        main.searching = false;
+        main.end = false;
 
-        ctrl.getMatchedMenuItems = function() {
-            MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
+        main.getMatchedMenuItems = function() {
+            main.found = [];
+            main.end = false;
+
+            if (!main.searchTerm) {
+                main.end = true;
+                return;
+            }
+
+            main.searching = true;
+            MenuSearchService.getMatchedMenuItems(main.searchTerm)
                 .then(function(response) {
-                    ctrl.found = response;
-                    ctrl.noItemFound = ctrl.found.length == 0;
+                    main.found = response;
+                    main.searching = false;
+                    main.end = true;
+                })
+                .catch(function(error) {
+                    main.found = [];
+                    main.searching = false;
+                    main.end = true;
                 });
         }
 
-        ctrl.onRemove = function(index) {
-            ctrl.found.splice(index, 1);
+        main.onRemove = function(index) {
+            main.found.splice(index, 1);
         }
     }
-
+    ///////////////
+    // SERVICE:     MenuSearchService
+    //////////////
     MenuSearchService.inject = ['$http', 'ApiBasePath'];
+
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
 
@@ -66,8 +126,6 @@
                     service.menuItems = result.data.menu_items;
 
                     function filterMenu(el) {
-                        if(!searchTerm)
-                          return false;
                         return el.description.indexOf(searchTerm) !== -1;
                     }
 
@@ -75,6 +133,8 @@
 
                     // return processed items
                     return foundItems;
+                }, function(error) {
+                    return error;
                 });
         };
 
